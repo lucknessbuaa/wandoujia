230fd7ff4bcf78b87ef937d5a812a7455dfca347
lucknessbuaa <2573364901@qq.com>
2015-12-15 21:43:38 +0800

    first version

---
+++ constants.js
@@ -0,0 +1,6 @@
+var constants = {
+    'intro': '我们诚恳的邀请你参与豌豆荚的一览新功能内测：<br>绑定你的instagram账号，<br>从此即可在豌豆荚一览中轻松浏览你在instagram中关注的内容。<br><br><br>在首次绑定账号时需确保手机网络能稳定访问instagram呦：<br>',
+    'buttonVisit': '我可以访问instagram，下载豌豆荚一览内测版本',
+    'buttonUnvisit': '算了，我无法访问instagram',
+    'layerText': '请留下你的联系方式，若有版本更新，我们会第一时间通知你。'
+}
